6aa2900dae665f4d087827ece2d63368b81ea1dc
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2018-03-29 17:53:04 +0300

    Fix

---
+++ CreateDeviceIdentity/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
+{
+    internal class Program
+    {
+        private static RegistryManager _registryManager;
+        private static string _connectionString = "HostName=GredjaIoT.azure-devices.net;SharedAccessKeyName=GredjaIoT.azure-devices.net;SharedAccessKey=UlU/IIEXHNyk0Spz9bCw2ESyss2CjfR+ZhjGHEv4+78=";
+
+        private static void Main(string[] args)
+        {
+            _registryManager = RegistryManager.CreateFromConnectionString(_connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await _registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await _registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
---
+++ ReadDeviceToCloudMessages/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ReadDeviceToCloudMessages
+{
+    internal class Program
+    {
+        private static string _connectionString =
+                                        "HostName=GredjaIoT.azure-devices.net;SharedAccessKeyName=GredjaIoT.azure-devices.net;SharedAccessKey=UlU/IIEXHNyk0Spz9bCw2ESyss2CjfR+ZhjGHEv4+78="
+                        ;
+
+        private static string _iotHubD2CEndpoint = "messages/events";
+        private static EventHubClient _eventHubClient;
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Receive messages. Ctrl-C to exit.\n");
+            _eventHubClient = EventHubClient.CreateFromConnectionString(_connectionString, _iotHubD2CEndpoint);
+
+            var d2CPartitions = _eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            var cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+            var tasks = new List<Task>();
+
+            foreach (var partition in d2CPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            var eventHubReceiver = _eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
+            }
+        }
+
+    }
+}
